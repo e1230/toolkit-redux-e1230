42a28bad55d02265cfb062dd776c06c7ab55848e
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-26 17:21:54 -0500

    inicializar pokemon slice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,13 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>Pokemon</h1>
+      <hr />
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+      </ul>
+    </>
+  );
+};
