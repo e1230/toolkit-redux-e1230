af5e2e574d06266a5f4831f3270d5312cd2a1e56
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-26 18:25:47 -0500

    Introduccion axios. Mostrar pokemons paginadamente

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,23 +1,40 @@
+import { useState } from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon";
 
 export const PokemonApp = () => {
+  const [numberPage, setNumberPage] = useState(0);
   const dispatch = useDispatch();
+  const { isLoading, pokemons } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
-    dispatch(getPokemons());
-  }, []);
+    console.log("ejecutar");
+    dispatch(getPokemons(numberPage));
+  }, [numberPage]);
 
   return (
     <>
       <h1>Pokemon</h1>
       <hr />
-      <ul>
-        <li>a</li>
-        <li>b</li>
-        <li>c</li>
-      </ul>
+      {isLoading ? (
+        <span>loading</span>
+      ) : (
+        <ul>
+          {pokemons.map((pokemon) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ul>
+      )}
+
+      <button
+        disabled={isLoading}
+        onClick={() => {
+          setNumberPage(numberPage + 1);
+        }}
+      >
+        Siguiente
+      </button>
     </>
   );
 };
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,9 +1,12 @@
+import { pokemonApi } from "../../../api/PokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = (page = 0) => async (dispatch, getState) => {
-    dispatch(startLoadingPokemons);
+    dispatch(startLoadingPokemons());
 
     //TODO: Realizar petición HTTP
+    const { data } = await pokemonApi.get(`pokemon?limit=10&offset=${page * 10}`)
 
-    //dispatch(setPokemons);
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
 }
