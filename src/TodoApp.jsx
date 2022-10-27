7cb34f85cc056f300c279326533b5190aad85cfb
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-27 01:59:30 -0500

    Todo por Id con react RTK

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { useGetTodosQuery } from "./store/apis/todosApi";
+import { useState } from "react";
+import { useGetTodoByIdQuery, useGetTodosQuery } from "./store/apis/todosApi";
 
 export const TodoApp = () => {
-  const { data = [], isLoading } = useGetTodosQuery();
+  //const { data = [], isLoading } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1);
+  const { data, isLoading } = useGetTodoByIdQuery(todoId);
   return (
     <>
       <h1>Todos RTK - Query</h1>
       <hr />
       {isLoading ? <h4>is Loading </h4> : <div></div>}
 
-      <pre>...</pre>
-
-      <ul>
+      <pre>{JSON.stringify(data)}</pre>
+      {/* <ul>
         {data.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
-      </ul>
+      </ul> */}
 
-      <button>next ToDo</button>
+      <button
+        onClick={() => {
+          setTodoId(todoId - 1);
+        }}
+      >
+        prev ToDo
+      </button>
+      <button
+        onClick={() => {
+          setTodoId(todoId + 1);
+        }}
+      >
+        next ToDo
+      </button>
     </>
   );
 };
