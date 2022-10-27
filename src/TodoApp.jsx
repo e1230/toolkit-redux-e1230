f4dd7b1a1695844a66f81ed8c0a19c15c78f2b12
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-27 01:38:52 -0500

    consumiendo api RTK

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useGetTodosQuery } from "./store/apis/todosApi";
 
 export const TodoApp = () => {
+  const { data = [], isLoading } = useGetTodosQuery();
   return (
     <>
       <h1>Todos RTK - Query</h1>
       <hr />
-      <h4>is Loading</h4>
+      {isLoading ? <h4>is Loading </h4> : <div></div>}
 
       <pre>...</pre>
+
+      <ul>
+        {data.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
 
       <button>next ToDo</button>
     </>
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, } from '@reduxjs/toolkit'
+import { todosApi } from './apis/todosApi'
 import { counterSlice } from './slices/counter'
 import { pokemonSlice } from './slices/pokemon'
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
         pokemons: pokemonSlice.reducer,
+        [todosApi.reducerPath]: todosApi.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware)
 })
